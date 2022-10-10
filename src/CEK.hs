1ce67caa3b160404368e1793883269a1a3e2f00b
luciamr <lulita.martin@gmail.com>
2022-10-10 06:04:31 -0300

    Fix CEK, fallaba ClsFix

--- src/CEK.hs
+++ src/CEK.hs
@@ -33,7 +33,7 @@ type Kont = [Frame]
 runCEK :: MonadFD4 m => TTerm -> m TTerm
 runCEK tt = do
     v <- search tt [] []
-    return (val2TTerm v)
+    return $ val2TTerm v
 
 val2string :: Val -> String
 val2string (N n) = "N " ++ show n
@@ -92,17 +92,17 @@ destroy' v k = do
 
 destroy :: MonadFD4 m => Val -> Kont -> m Val
 destroy v [] = return v
-destroy (N i) ((FrmPrint s):k) = do printFD4 (s++show i)
-                                    destroy (N i) k
+destroy (N i) ((FrmPrint s):k) = do printFD4 (s++" "++show i)
+                                    destroy' (N i) k
 destroy (Cls _) ((FrmPrint s):k) = failFD4 "Error de tipo en runtime! : Print"
 destroy (N i) ((FrmBOpT e o t):k) = search' t e (FrmBOpV o (N i):k)
 destroy (N i2) ((FrmBOpV o (N i1)):k) =  destroy' (N (semOp o i1 i2)) k
 destroy (N _) ((FrmBOpV o (Cls _)):k) =  failFD4 "Error de tipo en runtime! : BinaryOp" -- TODO: pprint del operador
-destroy v ((FrmCls (ClsFix i e nm1 ty1 nm2 ty2 t)):k) = search t (Cls (ClsFix i e nm1 ty1 nm2 ty2 t):v:e) k
 destroy (N 0) ((FrmIfZ e tt te):k) = search' tt e k
 destroy (N _) ((FrmIfZ e tt te):k) = search' te e k
 destroy (Cls c) ((FrmApp e t):k) = search' t e (FrmCls c:k)
 destroy v ((FrmCls (ClsLam _ e nm ty t)):k) = search' t (v:e) k
+destroy v ((FrmCls c@(ClsFix i e nm1 ty1 nm2 ty2 t)):k) = search' t (v:Cls c:e) k
 destroy v ((FrmLet e nm t):k) = search' t (v:e) k
 
 val2TTerm :: Val -> TTerm
