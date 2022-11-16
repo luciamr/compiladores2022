47418db15d9c11f057fdf1fbe7e6febae9d5a136
luciamr <lulita.martin@gmail.com>
2022-11-16 02:16:56 -0300

    Sync

--- app/Main.hs
+++ app/Main.hs
@@ -50,6 +50,7 @@ parseMode = (,) <$>
   -- <|> flag' Bytecompile (long "bytecompile" <> short 'm' <> help "Compilar a la BVM")
   -- <|> flag' RunVM (long "runVM" <> short 'r' <> help "Ejecutar bytecode en la BVM")
       <|> flag Interactive Interactive ( long "interactive" <> short 'i' <> help "Ejecutar en forma interactiva")
+      <|> flag Eval        Eval        (long "eval" <> short 'e' <> help "Evaluar programa")
   -- <|> flag' CC ( long "cc" <> short 'c' <> help "Compilar a código C")
   -- <|> flag' Canon ( long "canon" <> short 'n' <> help "Imprimir canonicalización")
   -- <|> flag' Assembler ( long "assembler" <> short 'a' <> help "Imprimir Assembler resultante")
@@ -90,6 +91,7 @@ runOrFail c m = do
 
 repl :: (MonadFD4 m, MonadMask m) => [FilePath] -> InputT m ()
 repl args = do
+       lift $ setInter True
        lift $ catchErrors $ mapM_ compileFile args
        s <- lift get
        when (inter s) $ liftIO $ putStrLn
@@ -120,7 +122,7 @@ compileFile ::  MonadFD4 m => FilePath -> m ()
 compileFile f = do
     i <- getInter
     setInter False
-    printFD4 ("Abriendo "++f++"...")
+    when i $ printFD4 ("Abriendo "++f++"...")
     decls <- loadFile f
     mapM_ handleDecl decls
     setInter i
@@ -129,6 +131,11 @@ parseIO ::  MonadFD4 m => String -> P a -> String -> m a
 parseIO filename p x = case runP p x filename of
                   Left e  -> throwError (ParseErr e)
                   Right r -> return r
+
+evalDecl :: MonadFD4 m => Decl TTerm -> m (Decl TTerm)
+evalDecl (Decl p x e) = do
+    e' <- eval e
+    return (Decl p x e')
 
 handleDecl ::  MonadFD4 m => Decl STerm -> m ()
 handleDecl d = do
@@ -151,6 +158,12 @@ handleDecl d = do
               -- td' <- if opt then optimize td else td
               ppterm <- ppDecl td  --td'
               printFD4 ppterm
+
+          Eval -> do
+              td <- typecheckDecl d
+              -- td' <- if opt then optimizeDecl td else return td
+              ed <- evalDecl td
+              addDecl ed
 
       where
         typecheckDecl :: MonadFD4 m => Decl STerm -> m (Decl TTerm)
